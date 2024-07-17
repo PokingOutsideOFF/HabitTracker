8e2d824eb53019554c1d7a80daaa27856b743291
KaitoKid <shresawnt2011@gmail.com>
2024-07-17 16:03:28 +0530

    Build basic Habit Tracking DB

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,2 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using HabitTrackingDatabase;
+
+
+namespace HabitTracker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Menu();
+        }
+
+        void Menu()
+        {
+            string? choice = "";
+            try
+            {
+                do
+                {
+                    HabitDatabase db = new HabitDatabase();
+                    db.InitializeDatabse();
+
+                    Console.WriteLine("MAIN MENU");
+                    Console.WriteLine("\nWhat would you like to do\n");
+                    Console.WriteLine("Type 0 to Close Application");
+                    Console.WriteLine("Type 1 to View All Records");
+                    Console.WriteLine("Type 2 to Insert Record");
+                    Console.WriteLine("Type 3 to Delete Record");
+                    Console.WriteLine("Type 4 to Update Record");
+                    Console.WriteLine("----------------------------\n");
+                    Console.Write("Enter choice: ");
+                    choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "0":
+                            Console.WriteLine("Exiting........");
+                            break;
+                        case "1":
+                            db.ViewHabits();
+                            break;
+                        case "2":
+                            db.InsertHabits();
+                            break;
+                        case "3":
+                            db.DeleteHabits();
+                            break;
+                        case "4":
+                            db.UpdateHabits();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice");
+                            break;
+                    }
+                } while (choice != "0");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Opps error occured: " + ex);
+            }
+        }
+    }
+}
---
+++ HabitTrackingDatabase/HabitTrackingDatabase.cs
@@ -0,0 +1,141 @@
+﻿using System.Data.SQLite;
+
+namespace HabitTrackingDatabase
+{
+    public class HabitDatabase
+    {
+        public void InitializeDatabse()
+        {
+            using (var connection = new SQLiteConnection("Data Source= habits.db"))
+            {
+                connection.Open();
+                string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS habits(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       habit_name TEXT NOT NULL,
+                       quantity TEXT NOT NULL
+                    );";
+
+                using (var command = new SQLiteCommand(createTableQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void ViewHabits()
+        {
+            Console.WriteLine("YOUR HABITS");
+            Console.WriteLine("-----------------------");
+            using (var connection = new SQLiteConnection("Data Source= habits.db"))
+            {
+                connection.Open();
+                string selectQuery = "SELECT * FROM habits";
+                using(var command = new SQLiteCommand(selectQuery, connection))
+                {
+                    using(var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("ID\tHabit\tQuantity");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["id"]}\t{reader["habit_name"]}\t{reader["quantity"]}");
+                        }
+                    } 
+
+                }
+            }
+            Console.WriteLine("-----------------------\n");
+        }
+
+        public void InsertHabits()
+        {
+            Console.Write("Enter habit name: ");
+            string? habitName = Console.ReadLine();
+
+            Console.Write("Enter quantity: ");
+            string? habitQuantity = Console.ReadLine();
+            using(var connection = new SQLiteConnection("Data Source= habits.db"))
+            {
+                connection.Open();
+
+                string insertQuery = "INSERT INTO habits (habit_name, quantity) VALUES(@habit_name, @quantity)";
+                
+                using(var command = new SQLiteCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@habit_name", habitName);
+                    command.Parameters.AddWithValue("@quantity", habitQuantity);
+                    command.ExecuteNonQuery();
+                }
+            }
+            Console.WriteLine("Habit inserted succesfully.\n\n");
+        }
+
+        public void DeleteHabits()
+        {
+           
+            int id = 0;
+            while (true)
+            {
+                Console.Write("Enter habit id that is to be deleted: ");
+                if (int.TryParse(Console.ReadLine(), out id))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter valid integer");
+                }
+            }
+
+            using (var connection = new SQLiteConnection("Data Source= habits.db"))
+            {
+                connection.Open();
+                string deleteQuery = "DELETE FROM habits WHERE id = @id";
+                using (var command = new SQLiteCommand(deleteQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("\nHabit deleted successfully.\n\n");
+        }
+
+
+        public void UpdateHabits()
+        {
+            int id = 0;
+            while (true)
+            {
+                Console.Write("Enter habit id to be updated: ");
+                if(int.TryParse(Console.ReadLine(), out id))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter valid integer");
+                }
+            }
+
+
+            Console.Write("Enter new quantity: ");
+            string? newQuanity = Console.ReadLine();
+
+            using(var connection = new SQLiteConnection("Data Source = habits.db"))
+            {
+                connection.Open();
+                string updateQuery = "UPDATE habits SET quantity = @quantity WHERE id = @id";
+                using(var command = new SQLiteCommand(updateQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@quantity", newQuanity);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("\nHabit updated succesfully.\n\n");
+        }
+    }
+
+}
